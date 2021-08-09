b5606a16c9a55e553b5bb3f1cf99d58b4046cfd8
Deains <deinsid@gmail.com>
2021-08-10 00:09:18 +0300

    main action test

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+"""
+Module to run hello world
+"""
+
+
 def main():
-    print("Hello world!")
+    """
+    main function to run the application
+    """
+    print("Hello world")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
